8e9dcbbd5f8f6996bf026bd7a38b3c2bc37b060d
suchothendav <suchothendav@paypal.com>
2016-10-30 12:34:22 -0700

    inmemory db

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Path = require('path');
 const Express = require('express');
 const Kraken = require('kraken-js');
-//const InMemoryDB = require('./inmemorydb');
+const InMemoryDB = require('./inmemorydb');
 
 let options = {
     onconfig: (config, next) => {
@@ -10,7 +10,7 @@ let options = {
          * This is a simplified db only for tutorial/examples
          * For real world use case use MongoDB or Redis to store/retrieve data.
          */
-        //InMemoryDB.init();
+        InMemoryDB.init();
         next(null, config);
     }
 };
---
+++ inmemorydb/index.js
@@ -0,0 +1,67 @@
+/**
+ * This inmemory db is only for tutorial/example purpose.
+ * For real use case and production ready environment use products like redis, mongodb etc.
+ * This assumes
+ */
+const Loki = require('lokijs');
+let DB;
+let Users;
+let Pets;
+let Orders;
+let initialized = false;
+
+function createDb() {
+    if (!DB) {
+        DB = new Loki('Petstore');
+    }
+    return DB;
+}
+
+function init() {
+    if (initialized) {
+        return;
+    }
+    initialized = true;
+    let db = createDb();
+    Users = db.addCollection('users', { indices: ['id','username'] });
+    Pets = db.addCollection('pets', { indices: ['id','name'] });
+    Orders = db.addCollection('orders', { indices: ['id','petid'] });
+}
+
+function insertUser(user) {
+    let result;
+    if(!Users) {
+        return;
+    }
+    if (user && user.username) {
+        let currUser = findByUsername(user.username);
+        if (currUser && currUser.length > 0) {
+            currUser = Object.assign(currUser[0], user);
+            result = Users.update(currUser);
+        } else {
+            result = Users.insert(user);
+        }
+        return result;
+    }
+}
+
+function findAllUsers() {
+    if(!Users) {
+        return;
+    }
+    return Users.find();
+}
+
+function findUserByUsername(username) {
+    if(!Users) {
+        return;
+    }
+    return Users.find({'username': username})
+}
+
+module.exports = {
+    init: init,
+    insertUser: insertUser,
+    findUserByUsername: findUserByUsername,
+    findAllUsers:findAllUsers
+};
