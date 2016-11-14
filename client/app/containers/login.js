f0d428923f570d62a54c198f8bb6cb9a5a6152a1
suchothendav <suchothendav@paypal.com>
2016-11-13 23:06:34 -0800

    route middlewares

--- client/app/containers/login.js
+++ client/app/containers/login.js
@@ -4,6 +4,7 @@ import { connect as Connect } from 'react-redux';
 const Login = props => (
     <div>
         <a href="/v2/user/login" role="button" className="loginBtn center-block btn btn-lg btn-default">Login using Github</a>
+        <a href="#" role="button" className="addpetmargin loginBtn center-block btn btn-lg btn-default">Login using Twitter</a>
     </div>
 )
 
